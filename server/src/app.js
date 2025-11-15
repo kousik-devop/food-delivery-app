d19351318fe2e3c2f7bff7fa31162b60153a8db3
kousik-devop <kousikmaiti19@gmail.com>
2025-11-15 18:02:12 +0530

    Add Client link

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://food-delivery-app-wine-ten.vercel.app/",
     credentials: true
 }));
 app.use(cookieParser());
