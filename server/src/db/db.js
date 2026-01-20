5eff0f8605eb2c4ad8fb69dfa3d578d8f8dd30ca
kousik-devop <kousikmaiti19@gmail.com>
2026-01-20 20:32:25 +0530

    change

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => {
+    mongoose.connect(process.env.MONGODB_URI,).then(() => {
         console.log('Connected to MongoDB');
     }).catch((err) => {
         console.log(err);
