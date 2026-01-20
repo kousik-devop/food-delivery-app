eb4afced760d68e11b3cfb7f393695278f886f17
kousik-devop <kousikmaiti19@gmail.com>
2026-01-20 21:49:45 +0530

    chnage

--- server/src/app.js
+++ server/src/app.js
@@ -9,17 +9,12 @@ const orderRoutes = require('./routes/order.route');
 const cors = require('cors');
 const app = express();
 
-const cors = require("cors");
-
 app.use(cors({
   origin: "https://reeleat.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
-app.options("*", cors());
-
 
 
 app.use(cookieParser());
